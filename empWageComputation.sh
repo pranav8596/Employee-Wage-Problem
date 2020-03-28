b2a9eef148936bd42d8fda80583e0e61943f276f
pranav8596 <pige786123@gmail.com>
2020-03-28 16:15:09 +0530

    ADDED:Calculated daily employee wage with Case Statements

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,13 +6,15 @@ IS_FULL_TIME=2
 IS_PART_TIME=1
 
 randomCheck=$((RANDOM%3))
-if [ $randomCheck -eq $IS_FULL_TIME ]
-then
-	empHours=8
-elif [ $randomCheck -eq $IS_PART_TIME ]
-then
-	empHours=4
-else
-	empHours=0
-fi
+case $randomCheck in
+	$IS_FULL_TIME)
+		empHours=8
+		;;
+	$IS_PART_TIME)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
 dailyEmpWage=$(($empHours * $EMPLOYEE_RATE_PER_HOUR))
