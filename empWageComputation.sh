32243b2ffe21e3788123ed492e935b3587826c37
pranav8596 <pige786123@gmail.com>
2020-03-28 16:48:59 +0530

    ADDED:Calculated daily employee wages till Days & Hours reached to 20 and 100

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,10 +4,16 @@
 EMPLOYEE_RATE_PER_HOUR=20
 IS_FULL_TIME=2
 IS_PART_TIME=1
-WORKING_DAYS_PER_MONTH=20
+MAXIMUM_WORKING_DAYS=20
+MAXIMUM_WORKING_HOURS=100
 
-for (( day=1; day<=$WORKING_DAYS_PER_MONTH; day++ ))
+#Variables
+totalWorkingDays=0
+totalWorkingHours=0
+
+while [[ $totalWorkingDays -ne $MAXIMUM_WORKING_DAYS && $totalWorkingHours -ne $MAXIMUM_WORKING_HOURS ]]
 do
+	((totalWorkingDays++))
 	randomCheck=$((RANDOM%3))
 	case $randomCheck in
 		$IS_FULL_TIME)
@@ -20,6 +26,6 @@ do
 			empHours=0
 			;;
 	esac
-	salery=$(($empHours * $EMPLOYEE_RATE_PER_HOUR))
-	totalSalery=$(($totalSalery + $salery))
+	totalWorkingHours=$(($totalWorkingHours + $empHours))
 done
+totalSalery=$(($totalWorkingHours * $EMPLOYEE_RATE_PER_HOUR))
