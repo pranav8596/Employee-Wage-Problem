92bfd91139a75910efb4e392a38ae5dc5e7000d9
pranav8596 <pige786123@gmail.com>
2020-03-28 15:53:52 +0530

    ADDED:Calculated daily employee wage with Full time hour assumed as 8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
 
+#Constants
+EMPLOYEE_RATE_PER_HOUR=20
+
 isPresent=1
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
 	echo "Employee is Present"
+	empHours=8
 else
 	echo "Employee is Absent"
+	empHours=0
 fi
+dailyEmpWage=$(($empHours * $EMPLOYEE_RATE_PER_HOUR))
